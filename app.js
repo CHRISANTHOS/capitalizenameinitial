af2cf42fb3b0afe07adb502638ee9583f4fe8f2b
Akuchie Uchenna <uakuchie@gmail.com>
2021-08-04 12:24:04 +0100

    made adjustments for 3 names

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const name = prompt('enter name');
 const capitalize = (name) =>{
     let newName = name.split(' ');
     let firstName = newName[0];
-    let lastName = newName.length === 2? newName[1]: '' ;
+    let lastName = newName.length >= 2? newName[1]: '' ;
+    let otherName = newName.length > 2? newName[2]: '';
     let firstNameInitial = firstName[0].toUpperCase();
     let lastNameInitial = lastName.length? lastName[0].toUpperCase() : '';
-    const value = `${firstNameInitial}${firstName.slice(1)} ${lastNameInitial}${lastName.slice(1)}`
+    let otherNameInitial = otherName.length? otherName[0].toUpperCase() : '';
+    const value = `${firstNameInitial}${firstName.slice(1)} ${lastNameInitial}${lastName.slice(1)} ${otherNameInitial}${otherName.slice(1)}`
     return value;
 }
 console.log(capitalize(name));
